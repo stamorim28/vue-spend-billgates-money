8282fef25783daa6fd4a94bf70837285758b12a9
Stenio <jstenio.rocha@gmail.com>
2023-01-12 20:58:21 -0300

    feat(Price.vue) : add mixin formatAllMoney

--- src/components/Price.vue
+++ src/components/Price.vue
@@ -1,21 +1,19 @@
 <template>
   <div class="list__title">
     <h1>{{ prod }}</h1>
-    <h3>{{ `$${price}` }}</h3>
+    <h3>{{ formatAllMoney(price) }}</h3>
   </div>
 </template>
 
 <script>
+import { formatAllMoney } from "../mixins/formatAllMoney";
+
 export default {
   name: "Price",
+  mixins: [formatAllMoney],
   props: {
     price: Number,
     prod: String,
-  },
-  data() {
-    return {
-      num: 0,
-    };
   },
 };
 </script>
